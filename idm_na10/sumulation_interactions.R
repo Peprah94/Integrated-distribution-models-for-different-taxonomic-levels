f67c8a4ae086b91eef18c06e0f29813678b068ce
Peprah94 <31469850+Peprah94@users.noreply.github.com>
2021-11-04 14:50:51 +0100

    Initial data commit

---
+++ idm_na10/sumulation_interactions.R
@@ -0,0 +1,325 @@
+#Simulation of the data with replication at sites
+
+##############################################################################################
+#                         NOTES                                                              #
+##############################################################################################
+### We assume n.genus  g = 1,2,...,n.genus
+# For each g genera, we assume n.species in that geunus
+# We assume the data was collected at n.sites
+#We also assume the total number of visits to each site is n.visit.
+# We also assume that  some of the data are collected with species ID
+# Most of them are collected with genus ID
+#n.id = number of sites that have ID at the species level
+#n.gen = number of sites that have ID at the genus level
+#shan.index is the shannon's index
+#gamma is the detection probability for the abundance model (genus data)
+#p.tag is the detectiion probability for the occurence model (species data)
+#beta is a n.species* nsites matrix
+#includecov (T/F) indicates whether it is a model with a covariate (TRUE) or no covariate (FALSE)
+##############################################################################################
+set.seed(2020)
+myclog <- function(psi){
+  return(-log(1-psi))
+}
+
+myinvclog <- function(mu){
+  return(1-exp(-exp(mu)))
+}
+
+
+
+require(coda)
+require(nimble)
+require(devtools)
+require(mcmcplots)
+require(MCMCglmm)
+require(furrr)
+require(purrr)
+require(dplyr)
+require(stringr)
+require(MASS)
+require("LaplacesDemon")
+#library(rWishart)
+library(pbapply)
+
+
+sigma_sim <- function(n){
+  # S <- toeplitz((n:1)/n)
+  #S <- diag(n)
+  # R <- rWishart(1,(n+1),S, covariance=TRUE)
+  X <- matrix(rnorm(n^2,0,0.2), n, n) # Initialize empty matrix
+  m <- (t(X) + X)/2   # Make symmetric "m"
+  m_lower <- m[lower.tri(m, diag = FALSE)]
+  #m_lower
+  sig <- runif(n,0.2,1)
+  m2 <- matrix(0, n, n)
+  diag(m2) <- sig
+  m2[upper.tri(m2, diag = FALSE)] <- m_lower
+  cov <- t(m2) %*% m2
+  return(cov)
+}
+
+sig_new <- function(n, NLatent){
+  nu = 1; a1 = 2; b1=2; a2 = 2; b2=1; ns=n; nf=NLatent
+  set.seed(0)
+  Delta = matrix(c(rgamma(1,a1,b1), rgamma(nf-1,a2,b2)))
+  Psi = matrix(rgamma(nf*ns, nu/2, nu/2), ns, nf)
+  tau=apply(Delta,2,cumprod)
+  tauMat = t(matrix(tau, nf,ns))
+  Lambda=matrix(rnorm(nf*ns*sqrt(Psi*tauMat)^{-1}), ns, nf)
+  Lambda1 <- matrix(NA, nrow=ns, ncol=nf)
+
+  #sig_inv = rgamma(ns, 2,1)
+  sigma_inv = rep(0.1, ns)
+  Sigma = diag(sigma_inv)
+  
+  Omega = Lambda %*% t(Lambda)+Sigma
+  return(Omega)
+}
+
+sim <- function(input, seed){
+  n.sites = input$constants$n.sites
+  n.species = input$constants$n.species
+  n.visit = input$constants$n.visit
+  n.id = input$constants$n.id
+  n.gen = input$constants$n.gen
+  n.replicate= input$constants$n.replicate
+  q = input$constants$q
+  
+  set.seed(seed)
+  
+  #### CHECKS
+  
+  # is anything missing
+  
+  if(is.null(n.sites)){stop("please specify number of sites (n.sites)")}
+  if(is.null(n.species)){stop("please specify number of species (n.species)")}
+  if(is.null(n.visit)){stop("please specify number of visits (n.visit)")}
+  if(is.null(n.id)){stop("please specify number of sites that have ID at the species level (n.id)")}
+  if(is.null(n.gen)){stop("please specify number of species that have ID at the species level(n.gen)")}
+  #if(is.null(cov)){stop("please provide covariates (n.cov)")}
+  if(is.null(n.replicate)){stop("please number of replicates for the observation process")}
+  
+  # are all the elements of the dimensions correct?
+  
+  if((length(n.sites) == length(n.species)) == FALSE)
+  {stop("inputs(n.sites, n.species, n.visit) are not of the same length")}
+  
+  if((length(n.id) == length(n.gen)) == FALSE)
+  {stop("inputs( n.id, n.gen) are not of the same length")}
+  
+  if((length(input$parameters$ecological$beta0) !=n.species) & (length(input$parameters$ecological$beta1)!= n.species))
+  {stop("length of ecological parameters = number of species")}
+  
+  if((length(input$parameters$detection$alpha0) !=n.species) & (length(input$parameters$detection$alpha1)!= n.species))
+  {stop("length of detection parameters = number of species")}
+  
+  if(dim(input$interaction)[1] != dim(input$interaction)[1])
+  {stop("Dimension of interaction variance must be n.species * n.species")}
+  
+  # Dimension of matrix to store the various parameter values
+  
+  N <-log_lambda <- vector("numeric", n.sites)
+  z <-mu <- lambda.s <- epsilon <- eta <- p.tag <- matrix(NA, nrow = n.sites, ncol = n.species)
+  x <- array(NA, dim = c(n.sites, n.species, n.visit))
+  y <- matrix(NA, nrow=n.sites, ncol=n.visit)
+  
+  # Simulation of interaction
+  for(site.tag in 1:n.sites){
+    epsilon[site.tag,] = mvrnorm(1,rep(0, n.species),input$interaction )
+  }
+  
+  
+  #### Simulation of data 
+  #linear predictor for ecological process
+  for(site.tag in 1:n.sites){
+    for(spe.tag in 1:n.species){
+      mu[site.tag, spe.tag] = (input$parameters$ecological$beta0)[spe.tag] + (input$parameters$ecological$beta1)[spe.tag]* (input$covariate$ecological)[site.tag] + epsilon[site.tag,spe.tag]
+    }
+  }
+  
+  # estimation of occupancy probability
+  psi.s <- 1-exp(-exp(mu)) 
+  
+  # mean abundance
+  lambda.s <- exp(mu) 
+  #lambda.s <- -log(1-psi.s)
+  #psi.s <- 1-exp(-lambda.s) 
+  
+  #mean abundance for genus
+  lambda.g <- rowSums(lambda.s) 
+  
+  #detection probability.
+  for(site.tag in 1:n.sites){
+    for(spe.tag in 1:n.species){
+      eta[site.tag, spe.tag]= (input$parameters$detection$alpha0)[spe.tag] + (input$parameters$detection$alpha1)[spe.tag] * (input$covariate$detection)[site.tag]
+    }
+  }
+  p.tag <- invlogit(eta)
+  
+  #Simulation of observation process
+  for(site.tag in 1:n.sites){
+    for(spe.tag in 1:n.species){
+      #True presence or absence
+      z[site.tag, spe.tag] <- rbinom(1,1, psi.s[site.tag, spe.tag]) 
+      for(k in 1:n.visit){
+        # Genus counts
+        y[site.tag,k] <- rpois(1, lambda.g[site.tag])
+        # Species Presence and absence observations
+        x[site.tag, spe.tag,k] <- rbinom(1,n.replicate, z[site.tag, spe.tag]*p.tag[site.tag, spe.tag])
+      }
+    }
+  }
+  
+  # Making provision for the missing data at sites and species
+  for(k in 1:n.visit){
+    y[-(sample(1:n.sites, n.gen,replace=FALSE)),k] <- NA
+    for(spe.tag in 1:n.species){
+      x[-(sample(1:n.sites, n.id,replace=FALSE)), spe.tag,k] <- NA    
+    }
+  }
+  
+  pis <- lambda.s/lambda.g
+  
+  # Hill's number D for a given q
+  #hill_number <- (rowSums((lambda.s/lambda.g)^q))^(1/(1-q))
+  
+  #Shannon Index
+  #shan.index <- -rowSums(log(lambda.s/lambda.g)*(lambda.s/lambda.g), na.rm = TRUE)
+  
+  #Species richness
+  #richness <- log(rowSums((lambda.s/lambda.g)^0)^(1/(1-0)))
+  
+  #Eveness
+  #eveness <- shan.index/richness
+  
+  # Returning the results
+  data <- list(mat.species=x,
+               mat.genus = y,
+               #hill_number=hill_number, 
+               #shan.index=shan.index, 
+               #eveness=eveness,
+               #hill_index = q,
+               pis = pis,
+               ecological_cov =input$covariate$ecological ,
+               detection_cov = input$covariate$detection)
+  return(data)
+}
+
+
+#Simulation of replicates
+input10 <- list(
+  constants = list(
+    n.sites = 75,
+    n.species = 10,
+    n.visit = 3,
+    n.id= 45,
+    n.gen= 55,
+    n.replicate = 5,
+    q = 2
+  ) ,
+  covariate =list(
+    ecological = runif(75, -1,1),
+    detection = runif(75,-1,1)
+  ),
+  parameters = list(
+    ecological=list(
+     # beta0 = c(2, 1, 3, 2, 1, 2, 3, 1, 2, 2),
+      #beta1 = c(-2, 3, 1.5, 1, -1, 2, 2, 0, 0.5, -1.5)
+      beta0 = rnorm(10,0,1),
+      beta1=rnorm(10,0,1)
+    ),
+    detection=list(
+      alpha0 = rnorm(10,0,3),
+      alpha1 = rnorm(10,0.3)
+    )
+  ),
+  #interaction = sig_new(10,5)
+  #interaction = diag(10)
+  interaction = sigma_sim(10)
+)
+
+input20 <- list(
+  constants = list(
+    n.sites = 75,
+    n.species = 10,
+    n.visit = 3,
+    n.id= 45,
+    n.gen= 55,
+    n.replicate = 5,
+    q = 2
+  ) ,
+  covariate =list(
+    ecological = runif(75, -1,1),
+    detection = runif(75,-1,1)
+  ),
+  parameters = list(
+    ecological=list(
+      # beta0 = c(2, 1, 3, 2, 1, 2, 3, 1, 2, 2),
+      #beta1 = c(-2, 3, 1.5, 1, -1, 2, 2, 0, 0.5, -1.5)
+      beta0 = rnorm(10,0,1),
+      beta1=rnorm(10,0,1)
+    ),
+    detection=list(
+      alpha0 = rnorm(10,0,3),
+      alpha1 = rnorm(10,0.3)
+    )
+  ),
+  #interaction = sig_new(10,5)
+  #interaction = diag(10)
+  interaction = sigma_sim(10)
+)
+
+
+input30 <- list(
+  constants = list(
+    n.sites = 75,
+    n.species = 10,
+    n.visit = 3,
+    n.id= 45,
+    n.gen= 55,
+    n.replicate = 5,
+    q = 2
+  ) ,
+  covariate =list(
+    ecological = runif(75, -1,1),
+    detection = runif(75,-1,1)
+  ),
+  parameters = list(
+    ecological=list(
+      # beta0 = c(2, 1, 3, 2, 1, 2, 3, 1, 2, 2),
+      #beta1 = c(-2, 3, 1.5, 1, -1, 2, 2, 0, 0.5, -1.5)
+      beta0 = rnorm(10,0,1),
+      beta1=rnorm(10,0,1)
+    ),
+    detection=list(
+      alpha0 = rnorm(10,0,3),
+      alpha1 = rnorm(10,0.3)
+    )
+  ),
+  #interaction = sig_new(10,5)
+  #interaction = diag(10)
+  interaction = sigma_sim(10)
+)
+
+
+input_list_na <- list(input10,input20, input30)
+
+
+
+
+nreplicates <- 20
+simulations_all <- pblapply(input_list_na, function(x){
+  pblapply(1:nreplicates, function(z){
+  data <- sim(x, seed = z)
+  }, cl=4)
+}, cl=4)
+
+simulations_all_na <- flatten(simulations_all)
+
+#simulations_all =sim(input)
+save(simulations_all_na, file="sim_interractions_na.RData")
+save(input_list_na, file="sim_input_na.RData")
+
+
+
---
+++ idm_na20/nimble_genus_specific.R
@@ -0,0 +1,260 @@
+load("sim_interractions_na.RData")
+
+source("fnx_for estimation.R")
+
+require(coda)
+require(nimble)
+require(devtools)
+require(mcmcplots)
+require(MCMCglmm)
+require(furrr)
+require(purrr)
+require(future.apply)
+library(parallel)
+library(foreach)
+library(progressr)
+library(pbapply)
+
+handlers(global = TRUE)
+
+run_nimble_model <- function(simulations_all){
+  
+  # Set up
+  
+  #### Load required packages ####
+  require(coda)
+  require(nimble)
+  require(devtools)
+  require(mcmcplots)
+  require(MCMCglmm)
+  require(furrr)
+  require(purrr)
+  require(future.apply)
+  library(parallel)
+  library(foreach)
+  ############################################
+  code <- nimbleCode({
+    
+    #############################################################
+    #                 PRIOR DISTRIBUTIONS                       #
+    #############################################################
+    for(spe.tag in 1:n.species){
+      beta0[spe.tag]~ dnorm(0, sd=1) 
+      #mean.lambda[spe.tag] <- exp(beta0[spe.tag])
+      #beta0[spe.tag] <- log(mean.lambda[spe.tag])
+      #mean.lambda[spe.tag] ~ dunif(0,50)
+      alpha0[spe.tag] ~  dnorm(0,sd=10)
+      #mean.p[spe.tag] ~ dunif(0,1)
+      beta1[spe.tag] ~ dnorm(0,sd=10)
+      alpha1[spe.tag] ~ dnorm(0,sd=10)
+    }
+    
+    for(spe.tag in 1:n.species){
+      mu.eta[spe.tag] <- 0
+    }
+    
+    for(site.tag in 1:n.sites){
+      eta.lam[site.tag, 1:n.species] ~ dmnorm(mean = mu.eta[1:n.species],cov= Cov[1:n.species, 1:n.species])
+    }
+    
+    #Vague prior for variance covariance matrix
+    #omega[1:n.species,1:n.species] ~ dwish(R[1:n.species,1:n.species], df)
+    #sigma2[1:n.species,1:n.species] <- inverse(omega[1:n.species,1:n.species])
+    
+    #Priors for covariance matrix
+    a1 ~ dgamma(2,1)
+    a2 ~ dgamma(2,1)
+    delta[1] ~ dgamma(a1,1)
+    for(factor in 2:NLatent) {
+      delta[factor] ~ dgamma(a2,1)
+    }
+    
+    
+    for(l in 1:NLatent){
+      tauDelta[l] <- prod(delta[1:l])
+    }
+    
+    for(spe.tag in 1:n.species){
+      sig[spe.tag] ~ dgamma(a.sigma, b.sigma)
+    }
+    Sigma[1:n.species, 1:n.species] <- diag(1/sig[1:n.species])
+    
+    
+    
+    for (spe.tag in 1:n.species) {
+      for (l in 1:NLatent) {
+        phi[spe.tag,l] ~ dgamma(nu/2,nu/2)
+        tauFS[spe.tag, l] <- 1/(phi[spe.tag,l]*tauDelta[l])
+        lamLatent[spe.tag, l] ~ dnorm(mean = 0, sd= sqrt(tauFS[spe.tag,l]))
+      }
+    }
+    #Cov[1:n.species, 1:n.species] <- diag(n.species)
+    
+    
+    Cov[1:n.species, 1:n.species] <- lamLatent[1:n.species, 1:NLatent] %*% t(lamLatent[1:n.species, 1:NLatent]) + Sigma[1:n.species, 1:n.species]
+    for (spe.tag in 1:n.species) {
+      for (ss in 1:n.species) {
+        # Cov[spe.tag, ss] <- inprod(lamLatent[1:NLatent, spe.tag], lamLatent[1:NLatent, ss])+ Sigma[spe.tag,ss]
+        CorrIn[spe.tag, ss] <- Cov[spe.tag, ss]/sqrt(Cov[spe.tag, spe.tag] * Cov[ss, ss])
+      }
+    }
+    
+    precmatrix[1:n.species, 1:n.species] <- inverse(Cov[1:n.species, 1:n.species])
+    #Link between the abundance and occupancy
+    for(site.tag in 1:n.sites){
+      for(spe.tag in 1:n.species){
+        mu[site.tag,spe.tag] <- beta0[spe.tag] + beta1[spe.tag]*ecological_cov[site.tag]+eta.lam[site.tag,spe.tag]
+        log(lambda[site.tag, spe.tag]) <-  mu[site.tag,spe.tag]
+        #log(lambda[site.tag, spe.tag]) <-  cloglog(psi[site.tag, spe.tag])
+        #cloglog(psi[site.tag, spe.tag]) <-log(lambda[site.tag, spe.tag])
+        #cloglog(psi[site.tag, spe.tag]) <- mu[site.tag,spe.tag]
+      }
+    } 
+    
+    #Detection probability
+    for(site.tag in 1:n.sites){
+      for(spe.tag in 1:n.species){
+        logit(p.tag[site.tag, spe.tag]) <- alpha0[spe.tag] + alpha1[spe.tag]*detection_cov[site.tag]
+      }
+    }
+    
+
+    # Likelihood: key definitions in the likelihood
+    #############################################################
+    #                 Abundance Model                           #
+    #############################################################
+    
+    for(site.tag in 1:n.sites){
+      lambda.g[site.tag] <- sum(lambda[site.tag,1:n.species])
+      for(k in 1:n.visit){
+        Y[site.tag,k] ~ dpois(lambda.g[site.tag])
+      }
+    }
+    
+    #############################################################
+    #                pis                           #
+    #############################################################
+    
+    for(site.tag in 1:n.sites){
+      for(spe.tag in 1:n.species){
+        pis[site.tag, spe.tag] <- (lambda[site.tag,spe.tag]/lambda.g[site.tag])
+      }
+    }
+    #sumLogProb ~ dnorm(0,1)
+  })
+  
+  data <- simulations_all
+  #dimensions of the data
+  dim_data <- dim(data[[1]]) 
+  data_dim <- dim_data[2] 
+  
+  
+  
+  # Constants for the model 
+  const <- list(n.sites = dim_data[1], 
+                n.species= (dim_data[2]), 
+                n.replicates = 5,
+                n.visit=dim_data[3],
+                nu = 3,
+                NLatent=2,
+                a.sigma = 1,
+                b.sigma=1
+  )
+  
+  
+  #R and df
+  Rmat <- diag(const$n.species)
+  df <- const$n.species + 1 
+  #Data for the model
+  idm_data <- list(Y = simulations_all[[2]], 
+                   X = simulations_all[[1]], 
+                   detection_cov= data$detection_cov,
+                   ecological_cov = data$ecological_cov,
+                   R = Rmat,
+                   df = df)
+  
+  #Retrieving the true Presence/Absence
+  pa_data <- function(data){
+    zst <- apply(data, c(1,2), max)
+    zst[zst!= 0] <- 1
+    zst[is.na(zst)] <- 0
+    return(zst)
+  }
+  delta = rgamma(const$NLatent, 2,1)
+  phi =matrix(rgamma(const$n.species*const$NLatent,1.5,1.5), ncol=const$NLatent, nrow=const$n.species)
+  tau= cumprod(delta)
+  lamLatent = matrix(NA, ncol=const$NLatent, nrow=const$n.species)
+  
+  for(spe.tag in 1:const$n.species){
+    for(i in 1:const$NLatent){
+      lamLatent[spe.tag,i] <- rnorm(1,0,sd=sqrt(1/(phi[spe.tag,i]*tau[i])))
+    }
+  }
+  idm_inits <- function(){list(beta0= rnorm(const$n.species,0,1),
+                               beta1= rnorm(const$n.species,0,1),#covariate effect
+                               alpha0=rnorm(const$n.species,0,1), #detection probability
+                               alpha1=rnorm(const$n.species,0,1),
+                               #mean.p = rep(0.5,const$n.species),
+                               omega= diag(const$n.species),
+                               z=pa_data(idm_data$X),
+                               a1=2,
+                               a2=2,
+                               delta=delta,
+                               phi=phi,
+                               lamLatent= lamLatent ,
+                               sig = rgamma(const$n.species, 2,1)
+  )#True Presence/Absence
+  }
+  initsList <- idm_inits() 
+  
+  modelInfo <- list(
+    code = code,
+    constants = const,
+    data = idm_data,
+    inits = idm_inits
+  )
+  
+  #############################################################
+  #                 Compile Model                             #
+  #############################################################
+  
+  mwtc <- nimbleModel(code,
+                      name = "mwtc",
+                      data = idm_data, 
+                      constants = const, 
+                      inits = initsList)
+  Cmwtc <- compileNimble(mwtc)
+  
+  mcmcconf <- configureMCMC(Cmwtc, monitors = c("beta0", "beta1", "alpha0","alpha1", "pis","CorrIn"))
+  #mcmcconf <- configureMCMC(Cmwtc, monitors = c("beta0", "beta1", "alpha0","alpha1"))
+  
+  mcmcconf$removeSamplers(c("lamLatent", "phi","sig", "delta", "a1", "a2"))
+  mcmcconf$addSampler(c("lamLatent", "phi","sig", "delta", "a1", "a2"), "RW_block")
+  #mcmcconf$addSampler(c("lamLatent", "phi","sig", "delta", "a1", "a2"), "crossLevel")
+  
+  Rmcmc <- buildMCMC(mcmcconf)
+  cmcmc <- compileNimble(Rmcmc, project = Cmwtc,resetFunctions = TRUE) 
+  
+  estimate <- runMCMC(cmcmc,
+                      niter = 500000,
+                      nburnin = 400000,
+                      nchains=4,
+                      thin = 10,
+                      summary=TRUE,
+                      samples=TRUE,
+                      setSeed = TRUE,
+                      samplesAsCodaMCMC=TRUE)
+  
+  estimation <- estimate$summary$all.chains
+  est_fixed <- estimation[((const$n.species*const$n.species)+1):((const$n.species*const$n.species)+40),1]
+  est_pis <- matrix(estimation[,1][((const$n.species*const$n.species)+((const$n.species)*4)+1) : ((const$n.species*const$n.species)+((const$n.species)*4)+(const$n.sites*const$n.species) )], nrow=const$n.sites, ncol=const$n.species, byrow=FALSE)
+  return(list(est_pis, est_fixed, estimation))
+}
+
+## Run the simulations
+#run_nimble_model(simulations_all)
+cl <- makeCluster(5)
+setDefaultCluster(cl)
+genus_estimates_na <- pblapply(simulations_all_na, run_nimble_model, cl=cl)
+
+save(genus_estimates_na, file="estimate_genus_na.RData")
